1cb41d98971bca456d53f9916ffeef87b334bb75
Manikandan Prakash <140643626+M95kandan@users.noreply.github.com>
2024-05-02 21:19:38 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import random
+from flask import Flask
+
+app = Flask(__name__)
+
+STRINGS = ["Investments", "Smallcase", "Stocks", "buy-the-dip", "TickerTape"]
+
+@app.route("/api/v1", methods=["GET"])
+def get_random_string():
+    return random.choice(STRINGS)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8081)
